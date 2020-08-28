2ae00daedd8e324022c85a355a95c4fe9df9b48d
Federico Navarrete <darklord.navarrete@gmail.com>
2020-08-28 12:37:35 +0200

    Add project files.

---
+++ Xamarin-SmoothBottomBar-App/Fragments/FourthFragment.cs
@@ -0,0 +1,15 @@
+﻿using Android.OS;
+using Android.Views;
+using AndroidX.Fragment.App;
+
+namespace Xamarin_SmoothBottomBar_App.Fragments
+{
+    class FourthFragment : Fragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Inflate the layout for this fragment
+            return inflater.Inflate(Resource.Layout.fragment_fourth, container, false);
+        }
+    }
+}
---
+++ Xamarin-SmoothBottomBar-App/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.App;
+using AndroidX.Navigation;
+using ME.Ibrahimsn.Lib;
+
+namespace Xamarin_SmoothBottomBar_App
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private NavController NavController;
+        SmoothBottomBar BottomBar;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            try
+            {
+                base.OnCreate(savedInstanceState);
+                Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+                // Set our view from the "main" layout resource
+                SetContentView(Resource.Layout.activity_main);
+                BottomBar = FindViewById<SmoothBottomBar>(Resource.Id.bottomBar);
+                NavController = Navigation.FindNavController(this, Resource.Id.main_fragment);
+            }
+            catch (System.Exception ex)
+            {
+
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_bottom, menu);
+            if (menu != null)
+            {
+                BottomBar.SetupWithNavController(menu, NavController);
+            }
+
+            return true;
+        }
+
+        public override bool OnSupportNavigateUp()
+        {
+            NavController.NavigateUp();
+            return true;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
